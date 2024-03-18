6edc165c60acdc401e834efcdae34daf6f99a537
Kishan <kishanshukla53@gmail.com>
2024-03-18 14:05:02 +0530

    typescript ++

--- first.ts
+++ first.ts
@@ -17,7 +17,7 @@
 // function add(num1: number, num2: number): number {
 //     return num1 + num2; 
 //   }
-  
+
 
 // // defining object in javascript
 // const person = {
@@ -30,7 +30,7 @@
 //     name: string;
 //     age: number;
 //   }
-  
+
 //   const person: Person = {
 //     name: "Alice",
 //     age: 30
@@ -39,7 +39,7 @@
 // sum of two number 
 // function calculateSum(a:number,b:number):number {
 //     return a+b;
-    
+
 // }
 // let sum=calculateSum(5,2);
 // console.log(sum)
@@ -65,3 +65,65 @@
 // const result = calculate(10,10,"mul");
 // console.log(result);
 
+// create a function that greets with persons name and age
+
+// easy way
+// function greet(person:{
+// name:string,
+//     age:number
+// }):string {
+//     return "hello " + person.name + " your age is "+person.age;
+// }
+
+// console.log(greet({
+//     name:"kishan",
+//     age:21
+// }))
+
+// medium way
+// interface is like struct or class where b define
+// the variables 
+// interface person {
+//     name: string;
+//     age: number;
+// }
+
+// function greet(person: person): string {
+//     return "hello " + person.name + " your age is " + person.age;
+// }
+
+// console.log(greet({
+//     name: "kishan",
+//     age: 21
+// }))
+
+
+// we can create multiple data type
+interface genderprops{
+    gender : "male" | "female"; // we can write string also
+    pronouns : "mard" | "aurat";// we can write string also
+}
+
+interface person {
+    name: string;
+    age: number;
+    
+    // gender : genderprops; // we can do like this or like below
+    gender : {
+        gender : "male" | "female";
+        pronouns : "mard" | "aurat";
+    };
+}
+
+function greet(person: person): string {
+    return "hello " + person.name + " your age is " + person.age + " you are " + person.gender.gender + " and  you are " + person.gender.pronouns;
+}
+
+console.log(greet({
+    name: "kishan",
+    age: 21,
+    gender:{
+        gender:"male",
+        pronouns:"mard"
+    }
+}))
